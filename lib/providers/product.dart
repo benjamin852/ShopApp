264e34c994a7b49dd281048103d40b5bccc68fc6
Ben Weinberg <weinbergben33@gmail.com>
2020-01-27 17:15:52 -0500

    toggling favourites on server

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -18,8 +21,20 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavouriteStatus() {
+  void toggleFavouriteStatus() async {
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = 'https://shop-app-a0242.firebaseio.com/products/$id.json';
+    try {
+      await http.patch(
+        url,
+        body: json.encode({
+          'isFavourite': isFavourite,
+        }),
+      );
+    } catch (error) {
+      isFavourite = oldStatus;
+    }
   }
 }
